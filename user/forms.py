d6868859e5ea6035b696f8304baba6fe9dc02bb3
Dao Hong <mmtsd3333@gmail.com>
2025-01-17 01:38:26 +0900

    이메일로 로그인 방식으로 변경, Oauth와 기존 계정 충돌문제 해결

--- user/forms.py
+++ user/forms.py
@@ -2,10 +2,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Email'}),
+        label="Email"
+    )
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
         max_length=30,
-        help_text='150자 이하 문자, 숫자 그리고 @/./+/-/_만 가능합니다.'
+        # help_text='150자 이하 문자, 숫자 그리고 @/./+/-/_만 가능합니다.'
     )
     email = forms.EmailField(
         max_length=255,
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomAuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
 
@@ -8,10 +9,11 @@ def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save() 
-            username = form.cleaned_data.get('username')
+            user = form.save()  # 폼에서 사용자 생성: 이때 form은 이메일과 username 모두 받아야 함
+            # 예시로 form.cleaned_data에서 이메일을 가져옴
+            email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(email=email, password=raw_password)
             if user is not None:
                 login(request, user)
                 return redirect('game:dashboard')
@@ -21,22 +23,22 @@ def signup_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('user:login')
+    return redirect('game:dashboard')
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = CustomAuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('username')  # 여기서 'username'은 실제로 이메일 값입니다.
             password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = authenticate(email=email, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('game:dashboard') 
+                return redirect('game:dashboard')
             else:
                 return render(request, 'user/login.html', {'form': form, 'invalid_creds': True})
     else:
-        form = AuthenticationForm()
+        form = CustomAuthenticationForm()
     return render(request, 'user/login.html', {'form': form})
 
 @login_required
